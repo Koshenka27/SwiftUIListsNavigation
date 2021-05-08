e908b6fe71cb7c580e27ad85c8975de88df6f19b
Logan Koshenka <koshenka27@gmail.com>
2021-05-07 23:03:32 -0400

    Initial Commit

---
+++ listsAndNavDemo/listsAndNavDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  listsAndNavDemoApp.swift
+//  listsAndNavDemo
+//
+//  Created by Logan Koshenka on 5/7/21.
+//
+
+import SwiftUI
+
+@main
+struct listsAndNavDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
